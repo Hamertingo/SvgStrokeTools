6055682636cb2a882edbc3b15e1ca189861cb8d2
Hamertingo Mono Bola <137920960+Hamertingo@users.noreply.github.com>
2023-06-27 18:59:43 -0300

    Update hud.lua

--- SvgTools/essentials/hud.lua
+++ SvgTools/essentials/hud.lua
@@ -3,14 +3,15 @@ local posX, posY = getScreenStartPositionFromBox(89 * scale, 221 * scale, -50 * 
 
 -- Criando uma tabela de objetos SVGCache
 local strokes = {
-    [1] = SVGCache:new(57, 56, 'circle', {255, 0, 0}, false, 0.2, 2), -- Criação de um círculo vermelho
-    [2] = SVGCache:new(57, 56, 'circle', {255, 255, 255}, false, 0.2, 2) -- Criação de um círculo branco
+    [1] = SVGCache:new(100, 100, 'circle', {255, 0, 0}, false, 0.8, 2), -- Criação de um círculo vermelho
+    [2] = SVGCache:new(100, 100, 'polygon', {255, 255, 255}, true, 0.9, 2), -- Criação de um polygon branco
+    [3] = SVGCache:new(100, 100, 'rect', {255, 255, 255}, true, 0.6, 2), -- Criação de um retangulo branco
 }
 
 -- Adicionando um manipulador de eventos para renderização
 addEventHandler('onClientRender', root, function()
-    -- Atualizando o primeiro objeto SVGCache para representar a saúde do jogador
-    strokes[1]:update(posX - (50 * scale), posY + (40 * scale), 255, (getElementHealth(localPlayer) or 0))
-    -- Atualizando o segundo objeto SVGCache para representar a armadura do jogador
-    strokes[2]:update(posX - (50 * scale), posY + (40 * 3 * scale), 255, (getPedArmor(localPlayer) or 0))
+    -- Renderizando o SVG Strokes
+    strokes[1]:update(posX - (670 * scale), posY + (40 * scale), 255, getElementHealth(localPlayer))
+    strokes[2]:update(posX - (750 * scale), posY + (55 * 1 * scale), 255)
+    strokes[3]:update(posX - (550 * scale), posY + (40 * 1 * scale), 255)
 end)
